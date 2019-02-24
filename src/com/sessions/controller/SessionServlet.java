051ebd31b357b836a328963479d5f932d8b57d1d
Gary <gary00393@yahoo.com.tw>
2019-02-24 17:15:31 +0800

    gary修正分頁功能SessionsServlet

--- src/com/sessions/controller/SessionServlet.java
+++ src/com/sessions/controller/SessionServlet.java
@@ -27,9 +27,8 @@ import com.sessions.model.*;
 
 import ToolClasses.MyRequest;
 
-
 @WebServlet(name = "/sessions/SessionServlet", loadOnStartup = 1, urlPatterns = "/sessions/SessionServlet")
-public class SessionServlet extends HttpServlet{
+public class SessionServlet extends HttpServlet {
 	private static final long serialVersionUID = 6787696732983961662L;
 
 	private SessionsService sessionsService;
@@ -37,129 +36,134 @@ public class SessionServlet extends HttpServlet{
 	private Map<String, SessionsVO> map;
 	private Map<String, MovieInfoVO> movieMap;
 	private MovieInfoService movieInfoService;
-	
+
 	@Override
 	public void init() throws ServletException {
-		
+
 		sessionsService = new SessionsService();
 		movieInfoService = new MovieInfoService();
-		
+
 		list_all = new Vector<SessionsVO>();
 		map = new Hashtable<String, SessionsVO>();
 		movieMap = new Hashtable<String, MovieInfoVO>();
-		
+
 		ServletContext context = getServletContext();
-		
+
 		context.setAttribute("sessionList_all", list_all);
 		context.setAttribute("sessionMap", map);
 		context.setAttribute("movieMap", movieMap);
-		
+
 		freshSessions();
 		freshMovieInfoVO();
 	}
 
 	private void freshSessions() {
-		
+
 		list_all.clear();
-		
+
 		long time = System.currentTimeMillis();
-		
+
 		for (SessionsVO sessionsVO : sessionsService.getAll()) {
-			
+
 			list_all.add(sessionsVO);
-			
-			if(sessionsVO.getSessions_start().getTime() > time) {
-				map.put(sessionsVO.getSessions_no(), sessionsVO);	
+
+			if (sessionsVO.getSessions_start().getTime() > time) {
+				map.put(sessionsVO.getSessions_no(), sessionsVO);
 			}
-			
+
 		}
-		
-		
+
 	}
-	
+
 	private void freshMovieInfoVO() {
 		movieMap.clear();
-		
+
 		for (MovieInfoVO movieInfoVO : movieInfoService.getAll()) {
-			
+
 			movieMap.put(movieInfoVO.getMovie_no(), movieInfoVO);
 		}
-		
+
 	}
-	
-	
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		
+
 		req.setCharacterEncoding("UTF-8");
 		String action = req.getParameter("action");
-		
-		if("insertSessions".equals(action)) {
-			
+
+		if ("insertSessions".equals(action)) {
+
 			SessionsService ss = new SessionsService();
-			
+
 			String[] sessionss = req.getParameterValues("sessions");
-			
-			Map<String,CinemaVO> map = (Map<String,CinemaVO>)getServletContext().getAttribute("cinemaMap");
-			
+
+			Map<String, CinemaVO> map = (Map<String, CinemaVO>) getServletContext().getAttribute("cinemaMap");
+
 			for (String string : sessionss) {
-				
+
 				String[] strs = string.split(";");
-				
+
 				DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-				
+
 				Date udate = null;
-				
+
 				try {
 					udate = df.parse(strs[2]);
 				} catch (ParseException e) {
 					e.printStackTrace();
 				}
-				
-				ss.addSes(strs[0], strs[1], new java.sql.Timestamp(udate.getTime()), map.get(strs[1]).getCinema_type(), map.get(strs[1]).getCinema_size());
-				
+
+				ss.addSes(strs[0], strs[1], new java.sql.Timestamp(udate.getTime()), map.get(strs[1]).getCinema_type(),
+						map.get(strs[1]).getCinema_size());
+
 			}
-			
+
 			freshSessions();
-			
+
 			MyRequest myRequest = new MyRequest(req);
-			myRequest.setMyParameter("whichPage", "5");
-			
-			req.getRequestDispatcher("/Back_end/sessions/listAllSessions.jsp").forward(myRequest, res);
+
+			if ((list_all.size() % 10) == 0) {
+
+				myRequest.setMyParameter("whichPage", String.valueOf(list_all.size() / 10));
+
+			} else {
+
+				myRequest.setMyParameter("whichPage", String.valueOf((list_all.size() / 10) + 1));
+
+			}
+
+			req.getRequestDispatcher("/Back_end/sessions/listAllSessions.jsp?action=all").forward(myRequest, res);
 			return;
-			
+
 		}
-		
-		
+
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		
+
 		req.setCharacterEncoding("UTF-8");
 		String action = req.getParameter("action");
-		
+
 		long now = System.currentTimeMillis();
-		
-		
-		if("insert_need".equals(action)) {
-			
+
+		if ("insert_need".equals(action)) {
+
 			MovieInfoService ms = new MovieInfoService();
 			List<MovieInfoVO> movieInfoVOList = new ArrayList<MovieInfoVO>();
 			req.setAttribute("movieInfoVOList", movieInfoVOList);
-			
+
 			for (MovieInfoVO movieInfoVO : ms.getAll()) {
-				
-				if(movieInfoVO.getMovie_out().getTime() > now) {
-					movieInfoVOList.add(movieInfoVO);					
+
+				if (movieInfoVO.getMovie_out().getTime() > now) {
+					movieInfoVOList.add(movieInfoVO);
 				}
-				
-			}			
-			
+
+			}
+
 			req.getRequestDispatcher("/Back_end/sessions/insertSessions.jsp").forward(req, res);
-			
+
 		}
 	}
-	
-	
+
 }
