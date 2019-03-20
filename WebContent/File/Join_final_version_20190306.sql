efe0cb502b2c83970ac7ccc786d0ab207ba5423a
Kev <Kev@DESKTOP-OHM8ID0>
2019-03-20 23:43:00 +0800

    更改資料欄位

--- WebContent/File/Join_final_version_20190306.sql
+++ WebContent/File/Join_final_version_20190306.sql
@@ -391,7 +391,7 @@ CREATE TABLE MOVIEINFO(
     MOVIE_CAST          VARCHAR2(100CHAR)       NOT NULL,
     MOVIE_INTRO         CLOB                    ,
     MOVIE_LENGTH        VARCHAR2(10CHAR)        NOT NULL,
-    MOVIE_TRAILER       VARCHAR2(100CHAR)       ,
+    MOVIE_TRAILER       CLOB       ,
     MOVIE_PIC           BLOB                    ,
     MOVIE_IN            DATE                    NOT NULL,
     MOVIE_OUT           DATE                    NOT NULL,
